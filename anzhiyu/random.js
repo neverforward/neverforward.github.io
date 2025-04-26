835e937f1149fc1749a06065fa49a15114c6face
neverforward <neverforward@users.noreply.github.com>
2025-04-26 17:50:51 +0800

    Site updated: 2025-04-26 17:50:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/25/test/","2025/04/24/hello-world/"];function toRandomPost(){
+var posts=["2025/04/24/hello-world/","2025/04/25/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
