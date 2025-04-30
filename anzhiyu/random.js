c9194f8212565cc5283fd3ab681099975dd0cb47
neverforward <neverforward@users.noreply.github.com>
2025-04-30 22:24:32 +0800

    Site updated: 2025-04-30 22:24:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/25/test/","2025/04/26/HelloWorld/"];function toRandomPost(){
+var posts=["2025/04/26/HelloWorld/","2021/01/01/1 - 副本 (2)/","2021/01/01/1 - 副本 (6)/","2021/01/01/1/","2021/01/01/1 - 副本/","2021/01/01/1 - 副本 (3)/","2021/01/01/1 - 副本 (5)/","2021/01/01/1 - 副本 (4)/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
