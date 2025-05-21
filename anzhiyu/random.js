1e665c48afbe5d810933a6bee9122aa149fe0779
neverforward <neverforward@users.noreply.github.com>
2025-05-21 23:01:21 +0800

    Site updated: 2025-05-21 23:01:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/10/download-mc-bedrock/","2025/04/26/HelloWorld/","2025/05/14/download-mc-java/","2025/05/17/download-ts/","2025/05/17/download-vscode/","2025/05/14/sapi-1/"];function toRandomPost(){
+var posts=["2025/04/26/HelloWorld/","2025/05/10/download-mc-bedrock/","2025/05/14/download-mc-java/","2025/05/17/download-vscode/","2025/05/17/download-ts/","2025/05/14/sapi-1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
